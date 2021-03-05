e5643b6f17dd6b465aa876c4807addbba2c547fd
Oscar De Leon <owdelc@gmail.com>
2021-03-04 23:21:00 -0600

    1.3.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,20 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: false,
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),}
+
+};
+module: {
+  rules: [
+    {
+      test: /\.css/,
+      use: ["style-loader","css-loader"],
+    },
+  ],
+};
